0829c053025b1e677a324c732b2652f578981761
Maren Strobl <maren.strobl@cedis.fu-berlin.de>
2024-08-02 17:36:30 +0200

    Release 1.0.3, add javascript for accordion objects

--- assets/js/oes.js
+++ assets/js/oes.js
@@ -28,6 +28,12 @@ jQuery(document).ready(function () {
 
     /* Archive: check for pre-filter from url after loading page */
     oesFilter.filterFromURL();
+
+    /* Accordion */
+    jQuery(".oes-accordion").on("click", function (event) {
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("active");
+    });
 });
 
 /* Responsive --------------------------------------------------------------------------------------------------------*/
@@ -51,3 +57,49 @@ if (jQuery(window).width() < 992) {
             '\t\t\t\t</svg>');
     }
 }
+
+/* Print -------------------------------------------------------------------------------------------------------------*/
+window.matchMedia("print").addEventListener("change", evt => {
+
+    /* open all details blocks */
+    let elms;
+    if (evt.matches) {
+        elms = document.body.querySelectorAll("details:not([open])");
+        for (let e of elms) {
+            e.setAttribute("open", "");
+            e.dataset.wasclosed = "";
+        }
+    } else {
+        elms = document.body.querySelectorAll("details[data-wasclosed]");
+        for (let e of elms) {
+            e.removeAttribute("open");
+            delete e.dataset.wasclosed;
+        }
+    }
+
+    /* gallery */
+    if(evt.matches){
+        let gallery = document.body.querySelectorAll(".oes-panel-container-gallery");
+        for (let g of gallery){
+            let slider = jQuery(g).find('.oes-figure-slider-panel');
+            for (let s of slider){
+                for (let item of s.children){
+                    let itemImg = jQuery(item).find('img').get(0);
+                    if(itemImg) {
+                        const imgID = itemImg.dataset.id;
+                        let figcaption = jQuery(g).find('.oes-panel-figcaption-' + imgID).get(0);
+                        let newFigcaption = figcaption.cloneNode(true);
+                        jQuery(newFigcaption).addClass('active');
+                        jQuery(newFigcaption).addClass('oes-gallery-print');
+                        item.append(newFigcaption);
+                    }
+                }
+            }
+        }
+        
+    }
+    else{
+        let printElements = document.querySelectorAll('.oes-gallery-print');
+        for(let p of printElements) p.remove();
+    }
+})
